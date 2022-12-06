f0717f5ddcb502cba4d394e2d4e9203dd4ad8bcd
Joellensilva <joellen.absilva@gmail.com>
2022-12-06 14:24:43 -0300

    corrigindo teste

--- src/data_test.py
+++ src/data_test.py
@@ -13,7 +13,7 @@ class TestData(unittest.TestCase):
     def test_validate_existence(self):
         STATUS_DATA_UNAVAILABLE = 4
         with self.assertRaises(SystemExit) as cm:
-            dados = load(file_names, "2021", "01", "/output")
+            dados = load(file_names, "2021", "01", "src/output_test")
             dados.validate("src/output_test/sheets/")
         self.assertEqual(cm.exception.code, STATUS_DATA_UNAVAILABLE)
 
